d434820d9ef339bb460fd4608e11ee52053bbcc0
Artem Bibaev <bibayev05@bk.ru>
2025-02-22 02:57:04 +0300

    Добавлено меню | Есть звуки кнопок | Есть плавный переход на сцену

---
+++ Main_menu/menu.gd
@@ -0,0 +1,6 @@
+extends ParallaxBackground
+
+var speed: int = 80
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scroll_offset.x -= speed * delta
